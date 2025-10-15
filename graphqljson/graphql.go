020e48daa38d564cd3e3de6d1810708b1e3b18c8
sonatard <sonatard12@gmail.com>
2025-10-15 10:18:28 +0900

    Reflectの削除

--- graphqljson/graphql.go
+++ graphqljson/graphql.go
@@ -2,7 +2,6 @@ package graphqljson
 
 import (
 	"fmt"
-	"reflect"
 
 	"encoding/json/jsontext"
 	json "encoding/json/v2"
@@ -11,11 +10,6 @@ import (
 // UnmarshalData parses the GraphQL response payload contained in data and stores
 // the result into v, which must be a non-nil pointer.
 func UnmarshalData(data jsontext.Value, v any) error {
-	rv := reflect.ValueOf(v)
-	if !rv.IsValid() || rv.Kind() != reflect.Pointer || rv.IsNil() {
-		return fmt.Errorf("decode graphql data: decode json: cannot decode into non-pointer %T", v)
-	}
-
 	if err := json.Unmarshal(data, v); err != nil {
 		return fmt.Errorf("decode graphql data: decode json: %w", err)
 	}
